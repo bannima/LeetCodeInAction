13fef2ee6ec4b01dafde48e9a1430354980e8a20
bannima <zhouenguo@126.com>
2021-02-20 10:49:16 +0800

    sliding windows method

--- sliding_window/剑指 Offer 57 - II和为s的连续正数序列/sliding_window.py
+++ sliding_window/剑指 Offer 57 - II和为s的连续正数序列/sliding_window.py
@@ -7,4 +7,22 @@ Author: Barry Chow
 Date: 2021/2/18 11:47 AM
 Version: 0.1
 """
+class Solution:
+    def findContinuousSequence(self, target: int) -> List[List[int]]:
+        sum = 0
+        l = 1;r=1
+        res = []
+        while l<=target//2:
+            if sum<target:
+                sum+=r
+                r+=1
+            elif sum>target:
+                sum-=l
+                l+=1
+            else:
+                res.append(list(range(l,r)))
+                sum-=l
+                l+=1
+
+        return res
 
