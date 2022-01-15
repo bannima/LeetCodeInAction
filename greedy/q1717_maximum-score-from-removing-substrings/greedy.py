4aa2253191dde2f9ef3fc8ca1a2fb3f9897ad27c
barryzhou <zhouenguo@126.com>
2022-01-15 20:40:17 +0800

    1717. 删除子字符串的最大得分

---
+++ greedy/q1717_maximum-score-from-removing-substrings/greedy.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@version: 0.1
+@author: zhouenguo
+@license: Apache Licence
+@file: greedy.py
+@time: 2022/1/15 8:38 PM
+@desc: 
+"""
+
+
+class Solution:
+    def maximumGain(self, s, x, y):
+        res = 0
+        def count(s, target, score):
+            nonlocal res
+            stack = []
+            for i in range(len(s)):
+                if stack and stack[-1] + s[i] == target:
+                    stack.pop()
+                    res += score
+                else:
+                    stack.append(s[i])
+            return stack
+
+        if x > y:#贪心找全部最大的字符串进行匹配，然后再找剩下的字符串进行匹配
+            stack = count(s, 'ab', x)
+            count(stack, 'ba', y)
+        else:
+            stack = count(s, 'ba', y)
+            count(stack, 'ab', x)
+        return res
