3c48642da7c1fbcc476e2f19a2e26ce0bb318179
barryzhou <zhouenguo@126.com>
2022-04-18 20:57:04 +0800

    203. 移除链表元素

---
+++ recursion/q203_remove-linked-list-elements/solution.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@version: 0.1
+@author: zhouenguo
+@license: Apache Licence
+@file: solution.py
+@time: 2022/4/18 8:56 PM
+@desc: 
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if not head:
+            return head
+        head.next = self.removeElements(head.next, val)
+        return head.next if head.val == val else head
+
