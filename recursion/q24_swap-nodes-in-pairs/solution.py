461d06412c5f6955aec26022f022aab94b4b524e
barryzhou <zhouenguo@126.com>
2022-04-18 20:06:08 +0800

    24. 两两交换链表中的节点

---
+++ recursion/q24_swap-nodes-in-pairs/solution.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@version: 0.1
+@author: zhouenguo
+@license: Apache Licence
+@file: solution.py
+@time: 2022/4/18 8:05 PM
+@desc: 
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+        next = head.next
+        head.next = self.swapPairs(next.next)
+        next.next = head
+
+        return next
