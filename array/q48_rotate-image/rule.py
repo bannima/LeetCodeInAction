986763af10292d453be5f0501f84b076acb6ac9f
barryzhou <zhouenguo@126.com>
2022-05-17 21:34:07 +0800

    48. 旋转图像

---
+++ array/q48_rotate-image/rule.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@version: 0.1
+@author: zhouenguo
+@license: Apache Licence
+@file: rule.py
+@time: 2022/5/17 9:33 PM
+@desc: 
+"""
+
+from typing import List
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n // 2):
+            for j in range(i, n - i - 1):
+                tmp = matrix[i][j]
+                matrix[i][j] = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = tmp
+
+        return matrix
+
